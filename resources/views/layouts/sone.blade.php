243422c967fced74c924e0101869f0ab28010b3f
Onur <odtulufb@hotmail.com>
2018-12-22 15:39:32 +0300

    elisyam 1.5 admin template added

---
+++ resources/views/layouts/sone.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="tr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>NetKeşif - Önmuhasebe</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Google Fonts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
+        <script>
+          WebFont.load({
+            google: {"families":["Montserrat:400,500,600,700","Noto+Sans:400,700"]},
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+          });
+        </script>
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ url('assets/img/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ url('assets/img/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/img/favicon-16x16.png') }}">
+        <!-- Stylesheet -->
+        <link rel="stylesheet" href="{{ url('assets/vendors/css/base/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ url('assets/vendors/css/base/elisyam-1.5.min.css') }}">
+        <link rel="stylesheet" href="{{ url('assets/css/animate/animate.min.css') }}">
+        <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    </head>
+    <body id="page-top">
+        <!-- Begin Preloader -->
+        <div id="preloader">
+            <div class="canvas">
+                <img src="{{ url('assets/img/logo.png') }}" alt="logo" class="loader-logo">
+                <div class="spinner"></div>   
+            </div>
+        </div>
+        <!-- End Preloader -->
+        <div class="page db-modern">
+            <!-- Begin Header -->
+            @include('partials.header')
+            <!-- End Header -->
+            <!-- Begin Page Content -->
+            <div class="page-content">
+                <!-- Begin Navigation -->
+                @include('partials.horizontalmenu')
+                <!-- End Navigation -->
+                <div class="content-inner boxed mt-4 w-100">
+                    <div class="container-fluid">
+                        @yield('content')
+                    </div>
+                    <!-- End Container -->
+                    <!-- Begin Page Footer-->
+                    @include('partials.footer')
+                    <!-- End Offcanvas Sidebar -->
+                </div>
+                <!-- End Content -->
+            </div>
+            <!-- End Page Content -->
+        </div>
+        <!-- Begin Vendor Js -->
+        <script src="{{ url('assets/vendors/js/base/jquery.min.js') }}"></script>
+        <script src="{{ url('assets/vendors/js/base/core.min.js') }}"></script>
+        <!-- End Vendor Js -->
+        <!-- Begin Page Vendor Js -->
+        <script src="{{ url('assets/vendors/js/nicescroll/nicescroll.min.js') }}"></script>
+        <script src="{{ url('assets/vendors/js/waypoints/waypoints.min.js') }}"></script>
+        <script src="{{ url('assets/vendors/js/chart/chart.min.js') }}"></script>
+        <script src="{{ url('assets/vendors/js/progress/circle-progress.min.js') }}"></script>
+        <script src="{{ url('assets/vendors/js/app/app.min.js') }}"></script>
+        <!-- End Page Vendor Js -->
+        <!-- Begin Page Snippets -->
+        <script src="{{ url('assets/js/dashboard/db-modern.min.js') }}"></script>
+        <!-- End Page Snippets -->
+    </body>
+</html>
---
+++ resources/views/partials/horizontalmenu.blade.php
@@ -0,0 +1,142 @@
+<div class="horizontal-menu">
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="navbar navbar-light navbar-expand-lg main-menu">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" href="#" id="dashboard" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dashboard</a>
+                            <ul class="dropdown-menu" aria-labelledby="dashboard">
+                                <li><a href="db-default.html">Default</a></li>
+                                <li><a href="db-clean.html">Clean</a></li>
+                                <li><a href="db-compact.html">Compact</a></li>
+                                <li><a href="db-modern.html">Modern</a></li>
+                                <li><a href="db-social.html">Social</a></li>
+                                <li><a href="db-smarthome.html">Smarthome</a></li>
+                                <li><a href="db-all.html">All</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" href="#" id="applications" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Applications</a>
+                            <ul class="dropdown-menu" aria-labelledby="applications">
+                                <li><a href="app-calendar.html">Calendar</a></li>
+                                <li><a href="app-chat.html">Chat</a></li>
+                                <li><a href="app-mail.html">Mail</a></li>
+                                <li><a href="app-contact.html">Contact</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" href="#" id="components" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Components</a>
+                            <ul class="dropdown-menu" aria-labelledby="components">
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="elements" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-share-alt"></i>UI Elements</a>
+                                    <ul class="dropdown-menu" aria-labelledby="elements">
+                                        <li><a href="components-buttons.html">Buttons</a></li>
+                                        <li><a href="components-alerts.html">Alerts</a></li>
+                                        <li><a href="components-modal.html">Modal</a></li>
+                                        <li><a href="components-notifications.html">Noty</a></li>
+                                        <li><a href="components-timeline.html">Timeline</a></li>
+                                        <li><a href="components-progress.html">Progress</a></li>
+                                        <li><a href="components-tabs.html">Tabs</a></li>
+                                        <li><a href="charts-chartjs.html">Chart Js</a></li>
+                                        <li><a href="components-accordion.html">Accordion</a></li>
+                                        <li><a href="components-popover.html">Popover</a></li>
+                                        <li><a href="components-tooltip.html">Tooltip</a></li>
+                                        <li><a href="components-scrollable.html">Scrollable</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="icons" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-font"></i>Icons</a>
+                                    <ul class="dropdown-menu" aria-labelledby="icons">
+                                        <li><a href="icons-lineawesome.html">Line Awesome</a></li>
+                                        <li><a href="icons-themify.html">Themify</a></li>
+                                        <li><a href="icons-ionicons.html">Ionicons</a></li>
+                                        <li><a href="icons-meteocons.html">Meteocons</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="forms" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-list-alt"></i>Forms</a>
+                                    <ul class="dropdown-menu" aria-labelledby="forms">
+                                        <li><a href="forms-basic.html">Form Basic</a></li>
+                                        <li><a href="forms-validation.html">Form Validation</a></li>
+                                        <li><a href="forms-wizard.html">Form Wizard</a></li>
+                                        <li><a href="forms-select.html">Bootstrap Select</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" id="tables" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-th-large"></i><span>Tables</span></a>
+                                    <ul class="dropdown-menu" aria-labelledby="tables">
+                                        <li><a href="tables-basic.html">Basic</a></li>
+                                        <li><a href="tables-datatables.html">Datatables</a></li>
+                                        <li><a href="tables-tabledit.html">Tabledit</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" href="#" id="pages" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
+                            <ul class="dropdown-menu" aria-labelledby="pages">
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="authentication" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-lock"></i>Authentication</a>
+                                    <ul class="dropdown-menu" aria-labelledby="authentication">
+                                        <li><a href="pages-login.html">Login</a></li>
+                                        <li><a href="pages-login-02.html">Login 02</a></li>
+                                        <li><a href="pages-register.html">Register</a></li>
+                                        <li><a href="pages-forgot-password.html">Forgot Password</a></li>
+                                        <li><a href="pages-lock-screen.html">Lock Screen</a></li>
+                                        <li><a href="pages-mail-confirm.html">Mail Confirmation</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="social" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-comments"></i>Social</a>
+                                    <ul class="dropdown-menu" aria-labelledby="social">
+                                        <li><a href="pages-newsfeed.html">Newsfeed</a></li>
+                                        <li><a href="pages-about.html">About</a></li>
+                                        <li><a href="pages-events.html">Events</a></li>
+                                        <li><a href="pages-friends.html">Friends</a></li>
+                                        <li><a href="pages-groups.html">Groups</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="email" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-at"></i>Email</a>
+                                    <ul class="dropdown-menu" aria-labelledby="email">
+                                        <li><a href="email-welcome.html">Welcome</a></li>
+                                        <li><a href="email-password.html">Reset Password</a></li>
+                                        <li><a href="email-order.html">Order Confirmation</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="errors" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-exclamation-triangle"></i>Errors</a>
+                                    <ul class="dropdown-menu" aria-labelledby="errors">
+                                        <li><a href="pages-404-01.html">Style 01</a></li>
+                                        <li><a href="pages-404-02.html">Style 02</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" href="#" id="pricing" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="la la-usd"></i>Pricing</a>
+                                    <ul class="dropdown-menu" aria-labelledby="pricing">
+                                        <li><a href="pages-pricing-tables-01.html">Style 01</a></li>
+                                        <li><a href="pages-pricing-tables-02.html">Style 02</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="pages-coming-soon.html"><i class="la la-rocket"></i>Coming Soon</a></li>
+                                <li><a href="pages-profile.html"><i class="la la-user"></i>Profile</a></li>
+                                <li><a href="pages-invoice.html"><i class="la la-clipboard"></i>Invoice</a></li>
+                                <li><a href="pages-search.html"><i class="la la-search"></i>Search</a></li>
+                                <li><a href="pages-faq.html"><i class="la la-question-circle"></i>FAQ</a></li>
+                                <li><a href="pages-blank.html"><i class="la la-edit"></i>Blank</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="maps-leaflet.html">Maps</a></li>
+                        <li><a href="components-widgets.html">Widgets</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
